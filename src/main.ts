b604ce840de630a27a29da389e59c25841e7aaad
Olin Kirkland <me@olinkirk.land>
2022-07-28 01:37:30 +0200

    Add cors?

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,16 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+// Use cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 connectToDatabase();
 
 // Determine if the password is correct
